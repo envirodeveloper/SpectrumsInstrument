a0da5c8dd8bce5222c30fa84ee396d544424c2c9
envirodeveloper <itadmin@technicrafts.com>
2025-06-25 13:12:30 +0530

    updating cors

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,14 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
-  methods: ["GET", "POST"],
-  credentials: true,
+  origin: [
+    "http://127.0.0.1:5500", 
+    "http://localhost:5500", 
+    "https://www.spectrumsinstrument.com"
+  ],
+  methods: ["GET", "POST", "OPTIONS"],
 }));
+
 
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
